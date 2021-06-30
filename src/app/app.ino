a8f55820737852071beb6257373bbba867b4f7da
Alejo <alejovillores@gmail.com>
2021-06-30 18:12:01 -0300

    carpeta de recursos y toque el codigo

--- src/app/app.ino
+++ src/app/app.ino
@@ -1,3 +1,13 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h> // Libreria para los sensores de temperaturas
+
+// Constantes del programa
+const float TEMPERATUR_RIEGO = 25.0; 
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,4 +16,9 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+
+bool temperaturaDeRiego(const float TEMPERATUR_RIEGO) {
+  float tempEnCelsius = 
 }
