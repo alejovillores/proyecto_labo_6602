6a7fa1869b434f788f02cc6e8e19c625d4240b63
Alejo <alejovillores@gmail.com>
2021-07-14 19:08:34 -0300

    cambios hechos en discord

--- src/app/app.ino
+++ src/app/app.ino
@@ -2,7 +2,10 @@
 #include "SparkFunBME280.h"
 
 // Constantes del programa
-const float TEMPERATUR_RIEGO = 25.0;
+const float TEMPERATURA_RIEGO = 25.0;
+const float HUMEDAD_RIEGO = 0.0;
+const float PRESION_RIEGO = 0.0;
+const int TIEMPO = 180000;
 
 BME280 bme280Sensor;
 
@@ -22,28 +25,34 @@ void setup() {
 }
 
 void loop() {
-  if (temperaturaDeRiego) {
-    if(humedadDeRiego && )
+  if (humedadDeRiego()) {
+    if( temperaturaDeRiego() || presionDeRiego()) {
+      activarRegador(TIEMPO);
+    }
   }
-
+  // Delay de 10 min
+  delay(600000)
 }
 
 
 // Evalua la temperatura actual con la que se determina con la constante
-bool temperaturaDeRiego(const float TEMPERATUR_RIEGO) {
+bool temperaturaDeRiego() {
   float tempCelsius = bme280Sensor.readTempC();
+  return tempCelsius >= TEMPERATURA_RIEGO;
+}
 
-  if (tempCelsius >= TEMPERATURA_RIEGO){
-    return true;
+// Evalua la humedad actual con la que se determina con la constante
+bool humedadDeRiego(){
+  float humedadMedida = analogRead(A0);
+ 
+  return humedadMedida >= HUMEDAD_RIEGO;
+}
+// Evalua la presion actual con la que se determina con la constante
+bool presionDeRiego(){
+  float presionMedida = bme280Sensor.readFloatPressure();
+  return presionMedida >= PRESION_RIEGO;
   }
 
-  return false;
-}
-
-bool humedadDeRiego(const float HUMEDAD_RIEGO){
+void activarRegador(const int TIEMPO){
   // code...
-  // TODO: ver el sensor de humedad en la tierra
-    
 }
-
-bool presionDeRiego(const float PRESION_RIEGO){}
